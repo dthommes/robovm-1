79d124d91c14984bef39f468bf03edbf089e8c7c
dkimitsa <demyan.kimitsa@gmail.com>
2017-11-09 11:15:58 +0200

    changed hex string to be capital this is required to signature futprint match one from provisioning profile

--- compiler/codesign/src/main/java/org/robovm/utils/codesign/utils/ByteBufferUtils.java
+++ compiler/codesign/src/main/java/org/robovm/utils/codesign/utils/ByteBufferUtils.java
@@ -80,7 +80,7 @@ public class ByteBufferUtils {
     public static String byteArrayToHex(byte[] a) {
         StringBuilder sb = new StringBuilder(a.length * 2);
         for(byte b: a)
-            sb.append(String.format("%02x", b));
+            sb.append(String.format("%02X", b));
         return sb.toString();
     }
 
