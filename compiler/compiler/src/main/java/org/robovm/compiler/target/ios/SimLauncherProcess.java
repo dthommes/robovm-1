eb25e13922600794a6b8f2099fb5fe212d226842
dkimitsa <demyan.kimitsa@gmail.com>
2020-05-21 18:17:45 +0300

    * watch support: added option to launch paired watch app from Idea

--- compiler/compiler/src/main/java/org/robovm/compiler/target/ios/SimLauncherProcess.java
+++ compiler/compiler/src/main/java/org/robovm/compiler/target/ios/SimLauncherProcess.java
@@ -63,7 +63,8 @@ public class SimLauncherProcess extends AbstractLauncherProcess<IOSSimulatorLaun
         }
 
         Executor executor;
-        if ("shutdown".equals(deviceType.getState(true).toLowerCase())) {
+        DeviceType freshState = deviceType.refresh();
+        if (freshState != null && "shutdown".equals(freshState.getState().toLowerCase())) {
             log.info("Booting simulator %s", deviceType.getUdid());
             executor = new Executor(log, "xcrun");
             executor.args("simctl", "boot", deviceType.getUdid());
@@ -81,6 +82,30 @@ public class SimLauncherProcess extends AbstractLauncherProcess<IOSSimulatorLaun
         executor = new Executor(log, "xcrun");
         executor.args("simctl", "install", deviceType.getUdid(), appDir.getAbsolutePath());
         executor.exec();
+
+        // launch and deploy to paired watch simulator
+        if (launchParameters.getPairedWatchAppName() != null && freshState != null  && freshState.getPair() != null) {
+            DeviceType watchDeviceType = freshState.getPair();
+            if ("shutdown".equals(watchDeviceType.getState().toLowerCase())) {
+                log.info("Booting watch simulator %s", watchDeviceType.getUdid());
+                executor = new Executor(log, "xcrun");
+                executor.args("simctl", "boot", watchDeviceType.getUdid());
+                executor.exec();
+            }
+
+            // bringing simulator to front (and showing it if it was just booted)
+            log.info("Showing watch simulator %s", watchDeviceType.getUdid());
+            executor = new Executor(log, "open");
+            executor.args("-a", "Simulator", "--args", "-CurrentDeviceUDID", watchDeviceType.getUdid());
+            executor.exec();
+
+            File watchAppDir = new File(appDir, "Watch/" + launchParameters.getPairedWatchAppName());
+            log.info("Deploying app %s to watch simulator %s", watchAppDir.getAbsolutePath(),
+                    watchDeviceType.getUdid());
+            executor = new Executor(log, "xcrun");
+            executor.args("simctl", "install", watchDeviceType.getUdid(), watchAppDir.getAbsolutePath());
+            executor.exec();
+        }
 
         log.info("Launching app %s on simulator %s", appDir.getAbsolutePath(),
                 deviceType.getUdid());
